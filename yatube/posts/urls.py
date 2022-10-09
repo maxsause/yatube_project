e11f14bb819d875632a4986fe53f2040d77e8242
Максим Белов <maxandmax337@gamil.com>
2022-10-09 18:42:01 +0300

    В теории рабочий сайт На практике не очень...

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.index),
+app_name = 'posts'
 
-    path('group/<slug:slug>/', views.group_posts),
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post, Group
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title = 'Группы'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, template, context)
 
 
-def group_posts(request, slug):
-    return HttpResponse(f'Посты, под группой {slug}')
+def group_posts(request, slug=None):
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
 
